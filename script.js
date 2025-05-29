ad2980e426c51d09f33d8ed92bd1e3753a274599
OtschoP <35844438+OtschoP@users.noreply.github.com>
2025-05-29 09:49:15 +0200

    added css and family font and header

--- script.js
+++ script.js
@@ -1,49 +1,45 @@
 const BASE_URL = "https://pokeapi.co/api/v2/"
 let pokemonList = [];
+let currentOffset = 0;
+
 function init() {
     fetchAllPokemons();
 }
 
-async function fetchAllPokemons() {
-    const response = await fetch(BASE_URL + "pokemon?limit=20&offset=0");
+async function fetchAllPokemons(currentOffset) {
+    const response = await fetch(BASE_URL + `pokemon?limit=20&offset=${currentOffset}`);
     const pokemonListData = await response.json();
-    pokemonListData.results.forEach(pokemon => {
-        fetchPokemonDetails(pokemon.url);
-    })
+    for (let pokemon of pokemonListData.results) {
+    await fetchPokemonDetails(pokemon.url);
+}
 }
 
 async function fetchPokemonDetails(url) {
     const response = await fetch(url);
     const pokemonDetails = await response.json();
     const types = pokemonDetails.types.map(entry => entry.type.name);
-    const image = pokemonDetails.other?.dream_world?.front_default || pokemonDetails.sprites.front_default;
+    //const image = pokemonDetails.other?.dream_world?.front_default || pokemonDetails.sprites.front_default;
+    const image = pokemonDetails.sprites.other.dream_world.front_default;
     const pokemon = {
+        id: pokemonDetails.id,
         name: pokemonDetails.name,
         image: image,
         types: types
     };
     pokemonList.push(pokemon);
-    console.log(pokemon);
+    console.log(pokemonDetails);
     renderPokemon(pokemon);
 
 }
 
+async function loadMorePokemons(){
+    currentOffset += 20; 
+    await fetchAllPokemons(currentOffset);
+}
+
 function renderPokemon(pokemon) {
     let html = document.getElementById('content');
-    html.innerHTML = getPokemonCardTemplate(pokemon);
+    html.innerHTML += getPokemonCardTemplate(pokemon);
 }
 
 
-function getPokemonCardTemplate(pokemon) {
-    return `
-<div class="pokeCard type-${pokemon.types[0]}">
-    <div class="pokeImage">
-        <img src="${pokemon.image}" alt="Bild von ${pokemon.name}">
-    </div>
-    <div class="pokeInfo">
-        <h2>${pokemon.name}</h2>
-        <div class="types">${pokemon.types.join(', ')}</div>
-    </div>
-</div>
-` 
-}
--- script/template.js
+++ script/template.js
@@ -1,10 +1,16 @@
-function getPokemonCardTemplate(){
+function getPokemonCardTemplate(pokemon) {
     return `
-    <div class="pokeCard">
-    <img src=${pokemon.image} alt="Bild eines Pokemon">
-    Name: ${pokemon.name}<br>
-    Type: ${pokemon.types}<br>
-
+<div class="pokeCard type-${pokemon.types[0]}">
+<div class="card-h2">
+<h2><span>#${pokemon.id}</span> <span>${pokemon.name}</span></h2>
+</div>
+    <div class="pokeImage">
+        <img src="${pokemon.image}" alt="Bild von ${pokemon.name}">
     </div>
-    `
+    <div class="pokeInfo">
+        
+        <div class="types"><strong><p>Type:</p></strong> <p>${pokemon.types.join(', ')}</p></div>
+    </div>
+</div>
+` 
 }
